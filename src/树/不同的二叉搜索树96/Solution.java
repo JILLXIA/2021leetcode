fe6a876fd68bb8468035b1520a3681e93d8265bd
JILLXIA <171250011@smail.nju.edu.cn>
2021-05-05 14:29:16 +0800

    2021.5.5

---
+++ src/树/不同的二叉搜索树96/Solution.java
@@ -0,0 +1,22 @@
+package 树.不同的二叉搜索树96;
+
+/**
+ * 非常明显的一道找规律的题（动态规划）
+ * 给你一个整数 n ，
+ * 求恰由 n 个节点组成且节点值从 1 到 n 互不相同的
+ * 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ */
+public class Solution {
+    public int numTrees(int n) {
+        int[] result = new int[n+1];
+        result[0] = 1;
+        result[1] = 1;
+
+        for(int i = 2;i<n+1;i++){
+            for(int j = 1;j<=i;j++){
+                result[i] += result[j-1]*result[i-j];
+            }
+        }
+        return result[n+1];
+    }
+}
