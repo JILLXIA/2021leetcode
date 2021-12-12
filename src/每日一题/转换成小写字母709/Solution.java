d2ea2a31975a80abfeb32ccc876c0ab4fd354ba2
JILLXIA <171250011@smail.nju.edu.cn>
2021-12-12 19:58:07 +0800

    2021.12.12

---
+++ src/每日一题/转换成小写字母709/Solution.java
@@ -0,0 +1,10 @@
+package 每日一题.转换成小写字母709;
+
+/**
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ */
+public class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
