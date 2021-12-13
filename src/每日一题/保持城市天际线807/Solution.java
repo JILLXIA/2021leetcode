6f354bc0f846b0bfb4790ee6aefb7c8a1e428216
JILLXIA <171250011@smail.nju.edu.cn>
2021-12-13 23:11:26 +0800

    2021.12.13

---
+++ src/每日一题/保持城市天际线807/Solution.java
@@ -0,0 +1,33 @@
+package 每日一题.保持城市天际线807;
+
+import java.util.Arrays;
+
+/**
+ * 给你一座由 n x n 个街区组成的城市，每个街区都包含一座立方体建筑。
+ * 给你一个下标从 0 开始的 n x n 整数矩阵 grid ，其中 grid[r][c] 表示坐落于 r 行 c 列的建筑物的 高度 。
+ *
+ * 城市的 天际线 是从远处观察城市时，所有建筑物形成的外部轮廓。从东、南、西、北四个主要方向观测到的 天际线 可能不同。
+ *
+ * 我们被允许为 任意数量的建筑物 的高度增加 任意增量（不同建筑物的增量可能不同） 。 高度为 0 的建筑物的高度也可以增加。然而，增加的建筑物高度 不能影响 从任何主要方向观察城市得到的 天际线 。
+ *
+ * 在 不改变 从任何主要方向观测到的城市 天际线 的前提下，返回建筑物可以增加的 最大高度增量总和 。
+ */
+public class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[][] arr = new int[grid.length][2];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                arr[i][0] = Math.max(grid[i][j],arr[i][0]);
+                arr[i][1] = Math.max(grid[j][i],arr[i][1]);
+            }
+        }
+        int result = 0;
+        for(int i = 0;i<n;i++) {
+            for (int j = 0; j < n; j++) {
+                result += Math.min(arr[i][0],arr[j][1]) - grid[i][j];
+            }
+        }
+        return result;
+    }
+}
