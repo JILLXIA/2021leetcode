53238dbe8ee5e7fd1dd2583f8c463259d9e3a596
JILLXIA <171250011@smail.nju.edu.cn>
2022-02-22 22:31:19 +0800

    20220217

--- src/动态规划/动态规划21天初级/第N天/不同路径62/Solution.java
+++ src/动态规划/动态规划21天初级/第N天/不同路径62/Solution.java
@@ -1,2 +1,23 @@
-package 动态规划.动态规划21天初级.第N天.不同路径62;public class Solution {
+package 动态规划.动态规划21天初级.第N天.不同路径62;
+
+/**
+ * 一个机器人位于一个 m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。
+ * 机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ *
+ * 问总共有多少条不同的路径？
+ */
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        long result = 1;
+        for(int i = n; i<= m+n-2;i++){
+            result = result * i/(i-n+1);
+        }
+        return (int)(result);
+    }
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.uniquePaths(23,12));
+    }
 }
