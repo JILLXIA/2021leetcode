e275463f7c2bb691d5e9e82ec55c34dd0d6c109e
JILLXIA <171250011@smail.nju.edu.cn>
2022-06-06 23:49:27 +0800

    20220606

---
+++ src/labuladong/ValidateBinarySearchTree98/Solution.java
@@ -0,0 +1,33 @@
+package labuladong.ValidateBinarySearchTree98;
+
+import 每日一题.TreeNode;
+
+/**
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * A valid BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ */
+public class Solution {
+	public boolean isValidBST(TreeNode root) {
+		return isValid(root,null,null);
+	}
+
+	public boolean isValid(TreeNode root, TreeNode min, TreeNode max){
+		if(root==null){
+			return true;
+		}
+
+		if(min!=null && root.val <= min.val){
+			return false;
+		}
+
+		if(max!=null && root.val >= max.val){
+			return false;
+		}
+		return isValid(root.left, min, root) && isValid(root.right, root, max);
+	}
+}
