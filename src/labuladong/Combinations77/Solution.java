2c5bdad95473361c4e4311664d21d913acf23a62
JILLXIA <171250011@smail.nju.edu.cn>
2022-07-22 19:37:17 +0800

    20220722

---
+++ src/labuladong/Combinations77/Solution.java
@@ -0,0 +1,33 @@
+package labuladong.Combinations77;
+
+import com.sun.tools.jdi.IntegerValueImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+ *
+ * You may return the answer in any order.
+ */
+public class Solution {
+	List<Integer> path = new ArrayList<>();
+	List<List<Integer>> result = new ArrayList<>();
+	public List<List<Integer>> combine(int n, int k) {
+		backTrace(n,k,1);
+		return result;
+	}
+
+	public void backTrace(int n, int k, int start){
+		if(path.size()==k){
+			result.add(new ArrayList<>(path));
+			return;
+		}
+
+		for(int i = start;i<=n;i++){
+			path.add(i);
+			backTrace(n,k,i+1);
+			path.remove(path.size()-1);
+		}
+	}
+}
