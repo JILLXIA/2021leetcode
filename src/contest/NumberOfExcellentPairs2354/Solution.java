9cfdf0744e4b252880b5e666f5be45d87fe0fb4a
JILLXIA <171250011@smail.nju.edu.cn>
2022-07-26 01:16:43 +0800

    20220725

---
+++ src/contest/NumberOfExcellentPairs2354/Solution.java
@@ -0,0 +1,54 @@
+package contest.NumberOfExcellentPairs2354;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution {
+	long result = 0;
+	List<Integer> path = new ArrayList<>();
+	public long countExcellentPairs(int[] nums, int k) {
+		// remove the repeat number in nums
+		boolean[] visited = new boolean[1000000001];
+		List<Integer> numArr = new ArrayList<>();
+		for(int i = 0;i<nums.length;i++){
+			if(!visited[nums[i]]){
+				numArr.add(nums[i]);
+				visited[nums[i]] = true;
+			}
+		}
+		// Collections.sort(numArr);
+		backtrace(numArr,k,0);
+		return result;
+	}
+
+	public void backtrace(List<Integer> numArr, int k, int start){
+		if(path.size()==2){
+			if(Integer.bitCount(path.get(0) | path.get(1)) +
+					Integer.bitCount(path.get(0) & path.get(1)) >= k){
+				if(path.get(0)==path.get(1)){
+					result += 1;
+				}else{
+					result += 2;
+				}
+			}
+			return;
+		}
+
+		for(int i = start;i<numArr.size();i++){
+			path.add(numArr.get(i));
+			backtrace(numArr,k,i);
+			path.remove(path.size()-1);
+		}
+	}
+
+	public int countNumber1(int num){
+		int count = 0;
+		while(num!=0){
+			num = num & (num-1);
+			count++;
+		}
+		return count;
+	}
+}
---
+++ src/contest/NumberOfExcellentPairs2354/Solution1.java
@@ -0,0 +1,40 @@
+package contest.NumberOfExcellentPairs2354;
+
+import java.util.HashSet;
+
+/**
+ * The Inclusion-Exclusion Principle
+ * bits(num1 OR num2) + bits(num1 AND num2) = bits(num1) + bits(num2)
+ *
+ * For all different a in nums,
+ * counts its number of bits.
+ *
+ * Enumearte the number of bits k1 and k2,
+ * if k1 + k2 >= k,
+ * we accumulate count[k1] * count[k2].
+ */
+public class Solution1 {
+	public long countExcellentPairs(int[] nums, int k) {
+		long[] count = new long[30];
+		long result = 0;
+		HashSet<Integer> set = new HashSet<>();
+		for(int i = 0;i<nums.length;i++){
+			if(!set.contains(nums[i])){
+				set.add(nums[i]);
+			}
+		} // dedup the nums
+
+		for(int tmp:set){
+			count[Integer.bitCount(tmp)]++;
+		}
+
+		for(int i = 0;i<count.length;i++){
+			for(int j = 0;j<count.length;j++){
+				if(i+j>=k){
+					result += count[i] * count[j];
+				}
+			}
+		}
+		return result;
+	}
+}
